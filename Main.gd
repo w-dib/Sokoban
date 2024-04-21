d56e0b8a083f01f65620680553b5d4f1f64f6107
Walid Daniel Dib <112762655+w-dib@users.noreply.github.com>
2024-04-21 22:05:52 +0400

    Add reset button

--- Main.gd
+++ Main.gd
@@ -1,15 +1,17 @@
-extends Node3D
+extends Node2D
 
-var cube_scene = preload("res://cube.tscn")
+@onready var texture_button: TextureButton = $CanvasLayer/TextureButton
 
-func _ready():
-	pass
 
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-			# Calculate the position to place the cube (simplified here for 0,0,0)
-				var cube = cube_scene.instantiate()
-				add_child(cube)
-				cube.global_position = Vector3(randi_range(-10,10), 0, randi_range(-10,10))  # You would calculate the correct position here
+func _on_texture_button_pressed() -> void:
+	get_tree().reload_current_scene()
+
+
+func _on_texture_button_mouse_entered() -> void:
+	texture_button.scale *= 1.1
+	
+
+
+
+func _on_texture_button_mouse_exited() -> void:
+	texture_button.scale /= 1.1
---
+++ goal.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+var tile_size = 64
+
+func _ready():
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size/2
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body is Crate:
+		print("Woohoo!")
+		
